25daa7d7eed77c423d19604d75ac526b7a1befe5
pepsiYoung <pozhenzi9010@live.com>
2019-06-11 21:56:46 +0800

    提交

---
+++ src/components/Chart.vue
@@ -0,0 +1,50 @@
+<template>
+  <div ref="chartDom" style="height:400px"></div>
+</template>
+
+<script>
+import echarts from "echarts";
+import { addListener, removeListener } from "resize-detector";
+import debounce from "lodash/debounce";
+export default {
+  created() {
+    this.resize = debounce(this.resize, 300);
+  },
+  mounted() {
+    // 基于准备好的dom，初始化echarts实例
+    this.chart = echarts.init(this.$refs.chartDom);
+    addListener(this.$refs.chartDom, this.resize);
+    // 绘制图表
+    this.chart.setOption({
+      title: {
+        text: "ECharts 入门示例"
+      },
+      tooltip: {},
+      xAxis: {
+        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+      },
+      yAxis: {},
+      series: [
+        {
+          name: "销量",
+          type: "bar",
+          data: [5, 20, 36, 10, 10, 20]
+        }
+      ]
+    });
+  },
+  beforeDestroy() {
+    removeListener(this.$refs.chartDom, this.resize);
+    this.chart.dispose();
+    this.chart = null;
+  },
+  methods: {
+    resize() {
+      console.log("resize");
+      this.chart.resize();
+    }
+  }
+};
+</script>
+
+<style></style>
--- src/views/Dashboard/Analysis.vue
+++ src/views/Dashboard/Analysis.vue
@@ -1,9 +1,16 @@
 <template>
-  <div>analysis</div>
+  <div>
+    <Chart />
+  </div>
 </template>
 
 <script>
-export default {};
+import Chart from "../../components/Chart";
+export default {
+  components: {
+    Chart
+  }
+};
 </script>
 
 <style></style>
